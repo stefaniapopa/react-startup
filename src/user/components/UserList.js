a0728c3b0b1ebb388092f45b5ced3ac679f724ef
Stefania Popa <stefania_florina10@yahoo.com>
2022-03-02 15:28:29 +0200

    create navigation components

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './UserList.css';
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserList = props => {
     if (props.items.length === 0){
-        return <div className="center">
-            <h2>No user found.</h2>
-        </div>
+        return (
+        <div className="center">
+            <Card>
+                <h2>No user found.</h2>
+            </Card>
+        </div>)
     }
 
 
